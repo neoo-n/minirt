90fbebd52af1ff562f3ce7aa0b3ec24efbede27f
milouw <dana.vauthey@gmail.com>
2025-06-29 17:37:49 +0200

    cone done

--- bonus/intersections/cone.c
+++ bonus/intersections/cone.c
@@ -6,27 +6,29 @@
 /*   By: dvauthey <dvauthey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 14:13:31 by dvauthey          #+#    #+#             */
-/*   Updated: 2025/06/29 16:33:07 by dvauthey         ###   ########.fr       */
+/*   Updated: 2025/06/29 17:31:35 by dvauthey         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "intersections.h"
 
-static double	cone_quadratic(t_coords ray, t_coords inverted_n,
-	t_coords edge_cam, double alpha)
+static double	cone_quadratic(t_coords ray, t_coords n,
+	t_coords cam_edge, double ratio)
 {
 	double	t;
-	double	dot_ray_in;
-	double	dot_edgecam_in;
-	double	cos_alpha;
+	double	dot_ray_n;
+	double	dot_camedge_n;
 
-	dot_ray_in = dot_prod(ray, inverted_n);
-	dot_edgecam_in = dot_prod(edge_cam, inverted_n);
-	cos_alpha = cos(alpha);
-	t = quadratic(dot_ray_in * dot_ray_in - (cos_alpha * cos_alpha), 2 * (dot_ray_in
-		* dot_edgecam_in - (dot_prod(ray, edge_cam) * cos_alpha * cos_alpha)),
-		dot_edgecam_in * dot_edgecam_in - (dot_prod(edge_cam, edge_cam)
-		* cos_alpha * cos_alpha));
+	dot_ray_n = dot_prod(ray, n);
+	dot_camedge_n = dot_prod(cam_edge, n);
+	// t = quadratic(dot_ray_in * dot_ray_in - (cos_alpha * cos_alpha), 2 * (dot_ray_in
+	// 	* dot_edgecam_in - (dot_prod(ray, edge_cam) * cos_alpha * cos_alpha)),
+	// 	dot_edgecam_in * dot_edgecam_in - (dot_prod(edge_cam, edge_cam)
+	// 	* cos_alpha * cos_alpha));
+	t = quadratic(dot_prod(ray, ray) - ratio * dot_ray_n * dot_ray_n,
+		2 * (dot_prod(cam_edge, ray) - (ratio * dot_camedge_n * dot_ray_n)
+		), dot_prod(cam_edge, cam_edge) - (ratio * dot_camedge_n *
+		dot_camedge_n));
 	return (t);
 }
 
@@ -47,20 +49,19 @@ double	cone_intersect(t_coords ray, t_coords origin, t_shape *cone, double t)
 	// t_coords	a;
 	// t_coords	b;
 	// t_coords	m;
-//	t_coords	inverted_n;
-	t_coords	edge_cam;
-	double		alpha;
+	t_coords	cam_edge;
+	double		ratio;
 	t_coords	p;
 	double		dist;
 
 	cone->vector = vect_normalised(cone->vector);
-//	inverted_n = vect_mult(cone->vector, -1);
-	edge_cam = vect_sub(vect_add(cone->coords, vect_mult(cone->vector, cone->height)), origin);
-	alpha = atan((cone->diam / 2) / cone->height);
+	cam_edge = vect_sub(origin, vect_add(cone->coords, vect_mult(cone->vector, cone->height)));
+	// alpha = atan((cone->diam / 2) / cone->height);
+	ratio = ((cone->diam / 2) / cone->height) + 1;
 	// m = vect_sub(origin, cone->coords);
 	// b = vect_sub(m, vect_mult(cone->vector, dot_prod(m, cone->vector)));
 	// a = vect_sub(ray, vect_mult(cone->vector, dot_prod(ray, cone->vector)));
-	t = cone_quadratic(ray, cone->vector, edge_cam, alpha);
+	t = cone_quadratic(ray, cone->vector, cam_edge, ratio);
 	// t = quadratic(dot_prod(a, a), 2 * dot_prod(a, b), (-1) * (dot_prod(b, b)
 	// 		- ((cone->diam * cone->diam / 4) * pow(dot_prod(ray, cone->vector) * cone->height, 2) / (cone->height * cone->height))));
 	if (t != -1)
